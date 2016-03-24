d98f4079299130a61bb2b5bff470637443ba1460
Tran Vinh Cuong <tranvinhcuong@gmail.com>
2016-03-24 12:44:40 +0900

    Changed mount point of main to attr.

--- app/__init__.py
+++ app/__init__.py
@@ -36,6 +36,6 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_blueprint, url_prefix='/attr')
 
     return app
